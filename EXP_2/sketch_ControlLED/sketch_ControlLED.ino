13e8ff33bd7636498cbf23e03732785bb1175da6
Jed <jed@jeddd.com>
2019-09-07 12:52:26 +0800

    Add feature to save LED brightness into EEPROM

--- EXP_2/sketch_ControlLED/sketch_ControlLED.ino
+++ EXP_2/sketch_ControlLED/sketch_ControlLED.ino
@@ -1,3 +1,5 @@
+#include <EEPROM.h>
+
 long read_int = 0;
 long brightness = 0;
 
@@ -5,10 +7,15 @@ void myPwm(int pin, unsigned long value, unsigned long duration=1, unsigned long
 
 
 void setup() {
+    // init
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
     Serial.setTimeout(50);
+    
+    brightness = EEPROM.read(0);  // load brightness value from EEPROM
     Serial.println("[+] Brightness is an integer between 0 and 10.");
+    Serial.print("Brightness: ");
+    Serial.println(brightness, DEC);
 }
 
 
@@ -19,6 +26,7 @@ void loop() {
             brightness = read_int;
             Serial.print("Brightness: ");
             Serial.println(brightness, DEC);
+            EEPROM.write(0, read_int);  // save brightness value to EEPROM
         }
         else {
             Serial.println("[-] Invalid value!");
@@ -38,9 +46,15 @@ void loop() {
 void myPwm(int pin, unsigned long value, unsigned long duration, unsigned long max_val) {
     unsigned long end_time = millis() + duration;
     while(millis() < end_time) {
-        digitalWrite(pin, HIGH);
-        delayMicroseconds(value);
-        digitalWrite(pin, LOW);
-        delayMicroseconds(max_val - value);
+        // 0 means only LOW, so there's no need to write HIGH
+        if (value > 0) {
+            digitalWrite(pin, HIGH);
+            delayMicroseconds(value);
+        }
+        // max_val means only HIGH, so there's no need to write LOW
+        if (value < max_val) {
+            digitalWrite(pin, LOW);
+            delayMicroseconds(max_val - value);
+        }
     }
 }
