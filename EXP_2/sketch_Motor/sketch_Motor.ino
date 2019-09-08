3c271dced8b67d3f6692fb2fb2c4822378a946ef
Jed <jed@jeddd.com>
2019-09-08 15:02:31 +0800

    Rearrange repo files

---
+++ EXP_2/sketch_Motor/sketch_Motor.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+#include <EEPROM.h>
+
+const int LEFT_EN = 11;     // 左边电机使能
+const int LEFT_SPDCTR = 5;  // 左边电机速度控制
+const int RIGHT_EN = 3;     // 右边电机使能
+const int RIGHT_SPDCTR = 6; // 右边电机速度控制
+
+Servo left_servo, right_servo;
+void setup() {
+    tes;
+}
+
+void loop() {
+    test;
+}
