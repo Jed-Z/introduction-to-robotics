e35c87533ebe0d861c586cb4cc7e39a29372d247
Jed <jed@jeddd.com>
2019-09-18 15:31:00 +0800

    Add EXP_3

---
+++ EXP_3/sketch_sensor/sketch_sensor.ino
@@ -0,0 +1,34 @@
+#define TRIG_PIN 2
+#define ECHO_PIN 3
+const int SOUND_SPEED = 340;  // sound speed in m/s
+
+float interval;  // time interval in microseconds
+float distance;  // distance in centimeters
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+    /* send a 10us HIGH pulse to trigger */
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    /* measure and convert */
+    interval = float(pulseIn(ECHO_PIN, HIGH));  // microseconds
+    distance = interval * SOUND_SPEED / 20000;  // centimeters
+
+    /* feedback */
+    Serial.print("Interval = ");
+    Serial.print(interval);//串口输出等待时间的原始数据
+    Serial.print("; Distance = ");
+    Serial.print(distance);//串口输出距离换算成cm的结果
+    Serial.println("cm");
+
+    delay(500);
+}
