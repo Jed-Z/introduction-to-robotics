c481cf80f99e8c81a25ab76ad013480f1aecfd12
Jed <jed@jeddd.com>
2019-10-16 20:38:06 +0800

    Update EXP_5 and add EXP_6

--- EXP_5/sketch_alongwall_old/define.h
+++ EXP_5/sketch_alongwall_old/define.h
@@ -8,20 +8,17 @@
 #define OLED_MOSI 13
 #define OLED_RESET 12
 
-// Encoder
+// SERVO pin
+#define SERVO 9
+//Encoder
 #define ENCODER_L 8 
 #define DIRECTION_L 4
 #define ENCODER_R 7
 #define DIRECTION_R 2
-
-
-// SERVO
-#define SERVO_PIN 9
-
 // Motor
-#define LDIRECTION_PIN 11
-#define RDIRECTION_PIN 3
-#define LSPEED_PIN 5
-#define RSPEED_PIN 6
+#define Ldirection 11
+#define Lspeed 5
+#define Rspeed 6
+#define Rdirection 3
 
 #endif
--- EXP_5/sketch_alongwall_old/sensor.h
+++ EXP_5/sketch_alongwall_old/sensor.h
@@ -1,11 +1,4 @@
-/* 电源电压 */
-double getVoltage() {
-    int read_int = analogRead(A0);              // 读电压原始值
-    double voltage = read_int * 5.371 / 100.0;  // 换算为伏特
-    return voltage;
-}
-
-/* 红外传感器：探测前方是否有障碍物（bool） */
+/* 用红外传感器探测前方是否有障碍物（bool） */
 bool getBarrier(int RED_PIN=12) {
     static bool is_inited = false;
     if(!is_inited) {
@@ -17,8 +10,8 @@ bool getBarrier(int RED_PIN=12) {
     return digitalRead(RED_PIN) == LOW;
 }
 
-/* 超声传感器：获取距离（厘米） */
-unsigned long getDistance(int TRIG_PIN=A1, int ECHO_PIN=A2) {
+/* 从超声传感器获取距离（厘米） */
+unsigned long getDistance(int TRIG_PIN=8, int ECHO_PIN=10) {
     static bool is_inited = false;
     if(!is_inited) {
         pinMode(TRIG_PIN, OUTPUT);
@@ -26,7 +19,7 @@ unsigned long getDistance(int TRIG_PIN=A1, int ECHO_PIN=A2) {
         is_inited = true;
     }
     
-    const int SOUND_SPEED = 340;  // 音速
+    const int SOUND_SPEED = 340;
     
     /* send a 10us HIGH pulse to trigger */
     digitalWrite(TRIG_PIN, LOW);
@@ -36,8 +29,8 @@ unsigned long getDistance(int TRIG_PIN=A1, int ECHO_PIN=A2) {
     digitalWrite(TRIG_PIN, LOW);
 
     /* measure and convert */
-    unsigned long interval = float(pulseIn(ECHO_PIN, HIGH));  // 微秒
-    unsigned long distance = interval * SOUND_SPEED / 20000;  // 厘米
+    unsigned long interval = float(pulseIn(ECHO_PIN, HIGH));  // microseconds
+    unsigned long distance = interval * SOUND_SPEED / 20000;  // centimeters
 
     return distance > 1000 ? 9999 : distance;  // 太大则认为无效
 }
